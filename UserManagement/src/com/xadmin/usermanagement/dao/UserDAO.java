24922ebe58c4675bac247f3c3a435e4ed92831c3
Suraj Sharma <112156492+Iamsuraj00@users.noreply.github.com>
2026-03-25 09:38:19 +0530

    initial commit

--- UserManagement/src/com/xadmin/usermanagement/dao/UserDAO.java
+++ UserManagement/src/com/xadmin/usermanagement/dao/UserDAO.java
@@ -13,7 +13,7 @@ import com.xadmin.usermanagement.model.User;
 public class UserDAO {
 	private String jdbcURL = "jdbc:mysql://localhost:3306/userdb?useSSL=false&serverTimezone=UTC";
 	private String jdbcUsername = "root";
-	private String jdbcPassword = "";
+	private String jdbcPassword = "root";
 
 	private static final String INSERT_USERS_SQL = "INSERT INTO users" + "  (name, email, country) VALUES "
 			+ " (?, ?, ?);";
